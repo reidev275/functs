dcd8545ff122b7a3b85677af9e4b5910f60a93a7
Reid Evans <reidev275@gmail.com>
2018-03-12 14:53:47 -0400

    either implements foldable

--- src/either.ts
+++ src/either.ts
@@ -1,6 +1,7 @@
-import { Fn } from "./common";
+import { Fn, Fn3 } from "./common";
+import { Foldable } from "./foldable";
 
-export interface Either<L, R> {
+export interface Either<L, R> extends Foldable<R> {
   map<U>(mapper: Fn<R, U>): Either<L, U>;
   chain<U>(chainer: Fn<R, Either<L, U>>): Either<L, U>;
 }
@@ -15,6 +16,10 @@ export class Left<L, R> implements Either<L, R> {
   chain<U>(chainer: Fn<R, Either<L, U>>): Either<L, U> {
     return new Left<L, U>(this.obj);
   }
+
+  reduce<A>(agg: Fn3<A, R, A>, empty: A): A {
+    return empty;
+  }
 }
 
 export class Right<L, R> implements Either<L, R> {
@@ -26,5 +31,9 @@ export class Right<L, R> implements Either<L, R> {
 
   chain<U>(chainer: Fn<R, Either<L, U>>): Either<L, U> {
     return chainer(this.obj);
+  }
+
+  reduce<A>(agg: Fn3<A, R, A>, empty: A): A {
+    return agg(empty, this.obj);
   }
 }
